f78ea79ac980b1a064f970ba1be97f40d1c2b609
SimonaCalentir79 <45466438+SimonaCalentir79@users.noreply.github.com>
2018-12-10 11:45:38 +0200

    Add files via upload

---
+++ Module4-OOP-TEMA01/SingleLinkedList/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleLinkedList
+{
+    class Program
+    {
+        static void Main()
+        {
+            SingleLinkedList list = new SingleLinkedList();
+            Node node1 = list.AddFirst(1);
+            Node node2 = list.AddAfter(node1, 2);
+            Node node3 = list.AddAfter(node2, 3);
+            Node node4 = list.AddAfter(node3, 4);
+            Node node5 = list.AddAfter(node4, 5);
+
+            node5.Next = node2;
+
+            if (list.DedectCycle() == true)
+                Console.WriteLine("\n Cycle detected!");
+            if (list.DedectCycle() == false)
+                list.Print();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Module4-OOP-TEMA01/SingleLinkedList/SingleLinkedList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleLinkedList
+{
+    class SingleLinkedList
+    {
+        private Node First;
+
+        public Node AddFirst(int value)
+        {
+            Node node = new Node();
+            node.Value = value;
+            node.Next = null;
+
+            First = node;
+
+            return node;
+        }
+        public Node AddAfter(Node node, int val)
+        {
+            Node newNode = new Node();
+            newNode.Value = val;
+            node.Next = newNode;
+
+            return newNode;
+        }
+
+        public bool DedectCycle()
+        {
+            bool esteCiclic = false;
+            Node currentNode = First;
+
+            List<Node> listaNoduri = new List<Node>();
+
+            while (currentNode != null)
+            {
+                listaNoduri.Add(currentNode);
+                if (listaNoduri.Contains(currentNode.Next))
+                {
+                    esteCiclic = true;
+                    break;
+                }
+                else
+                {
+                    currentNode = currentNode.Next;
+                    esteCiclic = false;
+                }
+            }
+            return esteCiclic;
+        }
+
+        public void Print()
+        {
+            Node currentNode = First;
+
+            while (currentNode != null)
+            {
+                Console.Write(currentNode.Value + " ");
+
+                currentNode = currentNode.Next;
+            }
+        }
+
+    }
+}
