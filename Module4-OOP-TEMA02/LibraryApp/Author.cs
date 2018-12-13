df7b5922d48af1995c0a3209dd2d2b6e27d56051
SimonaCalentir79 <45466438+SimonaCalentir79@users.noreply.github.com>
2018-12-13 23:32:00 +0200

    Add files via upload

---
+++ Module4-OOP-TEMA02/LibraryApp/Author.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp
+{
+    public class Author
+    {
+        public Author(string name, string email)
+        {
+            if (email.Contains("@"))
+            {
+                this.Name = name;
+                this.Email = email;
+            }
+            else
+            {
+                Console.WriteLine($"E-mail '{email}' doesn't have a valid format!!");
+            }
+        }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"\t\t\t Author name: '{this.Name}', e-mail: {this.Email}");
+        }
+    }
+}
